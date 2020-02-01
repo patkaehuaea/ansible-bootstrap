f72b187bad9bc97d2b93986801507c155a3b43b6
Pat Kaehuaea <pkaehuae@starbucks.com>
2020-01-31 19:15:26 -0800

    Pushed oh-my-zsh and powerline to ansible. removed from dotfiles because it was sad.

---
+++ bootstrap.sh
@@ -0,0 +1,8 @@
+sudo apt upgrade
+sudo apt update
+sudo apt install git
+sudo apt install vim
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible
+mkdir -p ~/src/ansible
+git clone https://github.com/patkaehuaea/ansible-bootstrap ~/src/ansible
