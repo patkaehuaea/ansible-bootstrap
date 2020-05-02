0dc3447fad7ee2a9d066eb0dfa16aea710ab3a86
Pat Kaehuaea <>
2020-05-02 00:09:45 -0700

    Removing functionality from ansible-role-zsh, ansible-role-vim, and ansible-role-dotfiles for simplicity. Workflow is much simpler if I include in bootstrap playbook.

--- bootstrap.sh
+++
@@ -1,8 +0,0 @@
-sudo apt upgrade
-sudo apt update
-sudo apt install git
-sudo apt install vim
-sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible
-mkdir -p ~/src/ansible
-git clone https://github.com/patkaehuaea/ansible-bootstrap ~/src/ansible
